be8da94a69a25b029adfdb4f9614a19ff9392af5
Donald Vlahovic <donald.vlahovic@cycos.com>
2015-05-04 12:33:10 +0200

    Basic Maven project created

---
+++ fitbit-connector/src/main/java/com/cycos/circuit/FitBitConnector.java
@@ -0,0 +1,8 @@
+package com.cycos.circuit;
+
+public class FitBitConnector 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ fitbit-connector/src/test/java/com/cycos/circuit/FitBitConnectorTest.java
@@ -0,0 +1,33 @@
+package com.cycos.circuit;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class FitBitConnectorTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public FitBitConnectorTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(FitBitConnectorTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testFitBitConnector() {
+        assertTrue(true);
+    }
+}
