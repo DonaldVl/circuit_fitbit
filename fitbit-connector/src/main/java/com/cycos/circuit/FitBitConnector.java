fbd35780bca56ad967b9bfd7ce01fe5f4ef64aaf
Donald Vlahovic <donald.vlahovic@cycos.com>
2015-05-05 11:47:34 +0200

    Authentication for FitBit implemented

--- fitbit-connector/src/main/java/com/cycos/circuit/FitBitConnector.java
+++ fitbit-connector/src/main/java/com/cycos/circuit/FitBitConnector.java
@@ -1,8 +1,28 @@
 package com.cycos.circuit;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+
+import com.fitbit.api.client.FitbitAPIEntityCache;
+import com.fitbit.api.client.FitbitApiClientAgent;
+import com.fitbit.api.client.FitbitApiCredentialsCache;
+import com.fitbit.api.client.FitbitApiCredentialsCacheMapImpl;
+import com.fitbit.api.client.FitbitApiEntityCacheMapImpl;
+import com.fitbit.api.client.FitbitApiSubscriptionStorage;
+import com.fitbit.api.client.FitbitApiSubscriptionStorageInMemoryImpl;
+import com.fitbit.api.client.service.FitbitAPIClientService;
+
 public class FitBitConnector 
 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	System.setProperty("http.proxySet", "true");
+    	System.setProperty("http.proxyHost", "proxy.cycos.com");
+    	System.setProperty("http.proxyPort", "8080");
+    	System.setProperty("https.proxyHost", "proxy.cycos.com");
+    	System.setProperty("https.proxyPort", "8080");
+    	FitBitConnection conn = new FitBitConnection();
+    	conn.init();
     }
 }
