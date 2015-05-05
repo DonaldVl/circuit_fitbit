e57198b648440e6f8c108389095177e1b8ce890a
Michael Rodenbuecher <michael.rodenbuecher@cycos.com>
2015-05-05 14:25:43 +0200

    Add Maven repository with internal libs

---
+++ fitbit-connector/src/test/java/com/cycos/circuit/CircuitConnection.java
@@ -0,0 +1,14 @@
+package com.cycos.circuit;
+
+import net.ansible.clientapi.testclient.client.SyncCircuitClient;
+
+import org.junit.Test;
+
+public class CircuitConnection {
+
+    @Test
+    public void checkMavenBuildAccess() {
+        SyncCircuitClient client;
+    }
+
+}
--- fitbit-connector/src/test/java/com/cycos/circuit/FitBitConnectorTest.java
+++ fitbit-connector/src/test/java/com/cycos/circuit/FitBitConnectorTest.java
@@ -1,33 +1,18 @@
 package com.cycos.circuit;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class FitBitConnectorTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public FitBitConnectorTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(FitBitConnectorTest.class);
-    }
+public class FitBitConnectorTest {
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testFitBitConnector() {
-        assertTrue(true);
+        Assert.assertTrue(true);
     }
 }
