45a30a21c601cfd272d8546a20538420dd8df84b
Armin Fink <armin.fink@cycos.com>
2015-05-06 16:47:20 +0200

    Adde implementation of circuit connector impl

--- circuit-connector/src/main/java/com/cycos/circuit/impl/CircuitConnectorImpl.java
+++ circuit-connector/src/main/java/com/cycos/circuit/impl/CircuitConnectorImpl.java
@@ -2,43 +2,150 @@ package com.cycos.circuit.impl;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import net.ansible.clientapi.testclient.client.SyncCircuitClient;
+import net.ansible.clientapi.testclient.connection.EventListener;
 import net.ansible.clientapi.testclient.websocket.WebsocketConnection;
+import net.ansible.protobuf.conversation.Conversation.ConversationType;
+import net.ansible.protobuf.conversation.ConversationEvent.EventType;
+import net.ansible.protobuf.conversation.ConversationItem;
+import net.ansible.protobuf.conversation.ConversationItem.TextItem;
+import net.ansible.protobuf.conversation.Participant;
+import net.ansible.protobuf.conversation.ConversationEvent.CreateConversationEvent;
+import net.ansible.protobuf.conversation.ConversationItem.ConversationItemType;
+import net.ansible.protobuf.conversation.ConversationItem.SystemItem;
+import net.ansible.protobuf.websocket.WSMessage;
+import net.ansible.protobuf.websocket.WSMessage.ContentType;
+import net.ansible.protobuf.websocket.WSMessage.Event;
 
 import com.cycos.circuit.CircuitConnector;
-import com.cycos.circuit.UserData;
+import com.cycos.circuit.CircuitData;
 import com.cycos.circuit.CircuitEventListener;
-import com.cycos.circuit.ConfigHandler;
 
-public class CircuitConnectorImpl implements CircuitConnector {
+public class CircuitConnectorImpl implements CircuitConnector, EventListener {
 
-	public List<UserData> getAllFitbitUsers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private final CircuitEventListener listener;
+    private final SyncCircuitClient client;
+    private final String userId;
 
-	public void createWelcomeTextItem(String conversationID) {
-		// TODO Auto-generated method stub
-		
-	}
+    public CircuitConnectorImpl(CircuitEventListener listener) throws URISyntaxException, Exception {
+        WebsocketConnection con = new WebsocketConnection(new URI("wss://localhost:8082/mock/ws"), true);
+        client = new SyncCircuitClient(con, "fitbit@unify.com", "abc123");
+        userId = client.user().getLoggedOnUser().getResponse().getUser().getGetLoggedOn().getUser().getUserId();
+        client.addEventListener(this);
+        this.listener = listener;
+    }
 
-	public void createURLTextItem(String conversationId, String url) {
-		// TODO Auto-generated method stub
-		
-	}
+    public List<CircuitData> getAllFitbitUsers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public void createTextItem(String conversationId, String text) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void createWelcomeTextItem(String conversationId) {
+        client.conversation().addTextItem(conversationId, "Welcome to fitbit instructor", "blalalalala", TextItem.ContentType.RICH, null, null, null);
+    }
 
-	public void createDirectConversation(String userId) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void createURLTextItem(String conversationId, String url) {
+        client.conversation().addTextItem(conversationId, null,
+                "Please click the link and follow the instruction. Afterwards come back and post your token with fitbit token 'MY_TOKEN'",
+                TextItem.ContentType.RICH, null, null, null);
 
-	
+    }
+
+    public void createTextItem(String conversationId, String text) {
+        client.conversation().addTextItem(conversationId, null, text, TextItem.ContentType.RICH, null, null, null);
+
+    }
+
+    public void createDirectConversation(String userId) {
+        client.conversation()
+                .create(Arrays.asList(new Participant[] { new Participant(userId) }), ConversationType.DIRECT, "FitBit private instructions", null);
+
+    }
+
+    public void eventReceived(WSMessage message) {
+        Event e = message.getEvent();
+        if (e.getType() == ContentType.CONVERSATION && e.getConversation() != null && e.getConversation().getCreate() != null) {
+            CreateConversationEvent create = e.getConversation().getCreate();
+
+            // Remove fitbit user
+            create.getConversation().getParticipantsList().remove(new Participant(userId));
+            if (e.getConversation().getCreate().getConversation().getType() == ConversationType.DIRECT) {
+
+                listener.onNewDirectConversation(create.getConversation().getConvId(), create.getConversation().getParticipantsList().get(0).getUserId());
+            } else {
+
+                ArrayList<String> users = new ArrayList<String>(create.getConversation().getParticipantsList().size());
+                for (Participant participant : create.getConversation().getParticipantsList()) {
+                    users.add(participant.getUserId());
+                }
+                listener.onNewDirectConversation(create.getConversation().getConvId(), users);
+            }
+
+        } else if (e.getType() == ContentType.CONVERSATION && e.getConversation() != null && e.getConversation().getAddItem() != null
+                && e.getConversation().getAddItem().getItem().getType() == ConversationItemType.SYSTEM
+                && e.getConversation().getAddItem().getItem().getSystem().getType() == SystemItem.ContentType.PARTICIPANT_ADDED) {
+
+            for (Participant p : e.getConversation().getAddItem().getItem().getSystem().getAffectedParticipantsList()) {
+                if (p.getUserId().equals(userId)) {
+                    ArrayList<String> users = new ArrayList<String>(e.getConversation().getAddItem().getItem().getSystem().getAffectedParticipantsList().size());
+                    for (Participant participant : e.getConversation().getAddItem().getItem().getSystem().getAffectedParticipantsList()) {
+                        users.add(participant.getUserId());
+                    }
+                    listener.onNewDirectConversation(e.getConversation().getConvId(), users);
+                }
+            }
+
+        } else if (EventType.ADD_ITEM.equals(message.getEvent().getConversation().getType())) {
+
+            if (!ignoreItem(message.getEvent().getConversation().getAddItem().getItem())) {
+
+                if (ConversationItemType.TEXT.equals(message.getEvent().getConversation().getAddItem().getItem().getType())) {
+                    String command = message.getEvent().getConversation().getAddItem().getItem().getText().getContent();
+                    if (isCommand(command)) {
+
+                        String creatorId = message.getEvent().getConversation().getAddItem().getItem().getCreatorId();
+
+                        if (command.startsWith("fitbit user")) {
+                            // User has given his Id
+                            listener.onNewFitbitUserId(creatorId, extractFitbitUserId(command));
+
+                        } else if (command.startsWith("fitbit food")) {
+                            // User has added new food entry
+                            listener.onNewFoodEntry(creatorId, extractFoodEntry(command));
+                        } else if (command.startsWith("fitbit token")) {
+                            // User has provided the token
+                            listener.onNewAuthenticationToken(creatorId, extractToken(command));
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    private String extractToken(String command) {
+        return command.substring("fitbit token ".length());
+    }
+
+    private String extractFoodEntry(String command) {
+        return command.substring("fitbit food ".length());
+    }
+
+    private String extractFitbitUserId(String command) {
+        return command.substring("fitbit user ".length());
+    }
+
+    private boolean isCommand(String command) {
+        return command.startsWith("fitbit");
+    }
+
+    private boolean ignoreItem(ConversationItem item) {
+        return userId.equals(item.getCreatorId());
+    }
 
 }
